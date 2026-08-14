5fc13127a822e2bfb81bb0a3ae2a5da9fd20dac4
Reilly Pidgeon <40374469+reillypidgeon@users.noreply.github.com>
2026-08-14 17:24:01 -0400

    Update mmseqs_search.sh

--- analysis_scripts/mmseqs_search.sh
+++ analysis_scripts/mmseqs_search.sh
@@ -7,21 +7,39 @@
 #SBATCH --mem=64G
 #SBATCH --cpus-per-task=4
 
+echo "Running $0"
+echo "This script adds genome accessions to the fasta header for use in downstream analyses that require this information."
+
+if [ "$#" -ne 2 ]; then
+    echo "Error: Invalid number of arguments."
+    echo "Required: FASTA files of the query and subject (target)"
+    echo "Usage: $0 <query_fasta> <subject_fasta>"
+    exit 1
+fi
+
 module load StdEnv/2023 mmseqs2/17-b804f cudacore/.12.6.3
 
+# Assign command line arguments
 query_fasta="$1"
 subject_fasta="$2" 
 
-out_format="query,target,pident,alnlen,mismatch,gapopen,qstart,qend,tstart,tend,evalue,bits,qseq,tseq"
-out_format="query,target,pident,alnlen,mismatch,gapopen,qstart,qend,tstart,tend,evalue,bits,qseq"
+if [ $subject_fasta == "*.faa" ]; then
+    echo "Protein subject/database"
+    out_format="query,target,pident,alnlen,mismatch,gapopen,qstart,qend,tstart,tend,evalue,bits,qseq,tseq"
+else
+    out_format="query,target,pident,alnlen,mismatch,gapopen,qstart,qend,tstart,tend,evalue,bits,qseq"
+fi
 
+# Run the search (auto-detects the input fasta formats)
 mmseqs easy-search \
     $query_fasta \
     $subject_fasta \
     mmseqs_results.tsv \
     tmp \
     --threads $SLURM_CPUS_PER_TASK \
-    --format-output "query,target,pident,alnlen,mismatch,gapopen,qstart,qend,tstart,tend,evalue,bits,qseq,tseq"
+    --format-output $out_format
 
 date
 echo "mmseqs search finished"
+
+# Add column headers based on the output format immediately
