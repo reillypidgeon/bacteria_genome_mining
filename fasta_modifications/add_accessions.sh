d6e36253a56f049eb3ce3833d6a44f62e75b26e7
Reilly Pidgeon <40374469+reillypidgeon@users.noreply.github.com>
2026-08-14 16:19:42 -0400

    Update add_accessions.sh

--- fasta_modifications/add_accessions.sh
+++ fasta_modifications/add_accessions.sh
@@ -1,1 +1,16 @@
+#!/usr/bin/env bash
+
+echo "Running $0"
+echo "This script adds genome accessions to the fasta header for use in downstream analyses that require this information."
+
+if [ "$#" -ne 2 ]; then
+    echo "Error: Invalid number of arguments."
+    echo "Required: A directory containing one or more fasta files"
+    echo "Usage: $0 <fasta_directory>"
+    exit 1
+fi
+
+# Define the accessions variable based on the user input file
+fasta_dir="$1"
+
 
