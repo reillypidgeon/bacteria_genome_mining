0a949bb75416b38c3bde838b86c2a887b0fb297e
Reilly Pidgeon <40374469+reillypidgeon@users.noreply.github.com>
2026-08-17 15:15:24 -0400

    Create pyrodigal_annotations.sh

---
+++ analysis_scripts/pyrodigal_annotations.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+#SBATCH --job-name=pyrodigal
+#SBATCH --time=12:00:00
+#SBATCH --mem=100G
+#SBATCH --cpus-per-task=8
+#SBATCH --output=%x_%j.out
+#SBATCH --error=%x_%j.err
+
+date
+set -euo pipefail
+
+# Load required modules
+module load StdEnv/2023 python/3.11.5
+echo "Modules loaded"
+
+echo "Running $0"
+echo "This script predicts proteins from one or more nucleotide fasta files using pyrodigal."
+
+if [ "$#" -lt 1 ]; then
+	echo "Error: Invalid number of arguments."
+	echo "Required: Nucleic acid fasta file(s)"
+	echo "Usage: $0 <fasta_fna file(s)>"
+	echo "Example: bash $0 genomes/*.fna"
+	exit 1
+fi
+
+# Create a virtual environment to load pyrodigal for parallel predictions
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+pip install --no-index --upgrade pip
+
+pip install --no-index -r pyrodigal_requirements.txt
+
+# Loop through the file(s)
+for fasta_file in "$@"; do
+	echo "Annotating ${fasta_file}"
+	
+	# Extract the path to the fasta file(s) and create an output directory
+	fasta_dir=$(dirname "${fasta_file}")
+	out_dir="${fasta_dir}/prodigal_out"
+	mkdir -p "${out_dir}"
+	
+	# Remove the trailing file extension and extract the fasta identity
+	lowercase_input=$(echo "${fasta_file}" | tr '[:upper:]' '[:lower:]')
+
+	if [[ "${fasta_file}" == *.fasta ]]; then
+		fasta_id=$(basename "$lowercase_input" .fasta)
+	elif [[ "${fasta_file}" == *.fna ]]; then
+		fasta_id=$(basename "$lowercase_input" .fna)
+	else
+		fasta_id=$(basename "$lowercase_input")
+	fi
+	
+	# Check if annotation already exists
+	if [ -f "${out_dir}/${fasta_id}_prodigal_prot.faa" ]; then
+		echo "Annotations already exist. Skipping..."
+		continue
+	fi
+	
+	pyrodigal -i "${fasta_file}" \
+	-p meta \
+	-g 11 \
+	-a "${out_dir}/${fasta_id}_prodigal_prot.faa" \
+	-d "${out_dir}/${fasta_id}_prodigal_gene.fna" \
+	-j $SLURM_CPUS_PER_TASK
+
+	echo "Finished running prodigal on ${fasta_id}"
+done
+
+date
