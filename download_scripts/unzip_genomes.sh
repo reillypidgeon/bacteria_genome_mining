198341fcbfb5f8a6ec77ec50e9a84e81872845b7
Reilly Pidgeon <40374469+reillypidgeon@users.noreply.github.com>
2026-09-01 15:41:08 -0400

    Update unzip_genomes.sh

--- download_scripts/unzip_genomes.sh
+++ download_scripts/unzip_genomes.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+echo "Running $0"
+echo "This script unzips fasta files downloaded from the NCBI."
+
+if [ "$#" -ne 1 ]; then
+    echo "Error: Invalid number of arguments."
+    echo "Required: A directory containing one or more fasta files"
+    echo "Usage: $0 <fasta_directory>"
+    exit 1
+fi
+
+# Define the accessions variable based on the user input file
+genomes_dir=$(basename "$1")
+cd "$genomes_dir"
+
 echo "Unzipping files"
 gunzip *.fna.gz
 echo "Unzipping finished"
