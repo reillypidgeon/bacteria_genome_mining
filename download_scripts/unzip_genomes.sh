d9ebd5d65a149dd0813d5765b50dd11bb3a07730
Reilly Pidgeon <40374469+reillypidgeon@users.noreply.github.com>
2026-09-01 14:59:11 -0400

    Update unzip_genomes.sh

--- download_scripts/unzip_genomes.sh
+++ download_scripts/unzip_genomes.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-#SBATCH --job-name=unzip_genomes
-#SBATCH --output=%x_%j.out
-#SBATCH --time=01:00:00
-#SBATCH --mem=32G
-
 echo "Unzipping files"
-date
 gunzip *.fna.gz
-sleep 5
 echo "Unzipping finished"
