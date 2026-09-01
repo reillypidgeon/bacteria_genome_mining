75cf91d00f9ed8240513bdfd1fce6b8d2c94a161
Reilly Pidgeon <40374469+reillypidgeon@users.noreply.github.com>
2026-09-01 11:42:56 -0400

    Update ncbi_genome_download.sh

--- download_scripts/ncbi_genome_download.sh
+++ download_scripts/ncbi_genome_download.sh
@@ -67,35 +67,8 @@ cat ../download_scripts/urls.txt | parallel -j 8 wget -nc
 
 echo "Finished downloading genomes"
 
+echo "Running unzip_genomes.sh"
 # Unzip the files
-sbatch --wait << 'EOF'
-#!/usr/bin/env bash
-
-#SBATCH --job-name=unzip_genomes
-#SBATCH --output=%x_%j.out
-#SBATCH --time=01:00:00
-#SBATCH --mem=32G
-
-echo "Unzipping files"
-date
-gunzip *.fna.gz
-sleep 5
-echo "Unzipping finished"
-
-cat unzip_genomes*
-# Remove the output file from the previous job to avoid errors when going through the genomes in later steps
-rm unzip_genomes*
-
-# Add the genome accessions to each fasta file
-if [ -f "../download_scripts/add_accessions.sh" ]; then
-    echo "add_accessions.sh script found"
-    bash ../download_scripts/add_accessions.sh .
-else
-    echo "add_accessions.sh script not found"
-    echo "Exiting script without any fasta modifications"
-    exit 1
-fi
-
-EOF
+sbatch --wait ../download_scipts/unzip_genomes.sh
 
 echo "Finished downloading and modifying the desired genomes"
