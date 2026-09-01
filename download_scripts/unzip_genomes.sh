1ac12f908ab698dbb524427ffb068368ea178abd
Reilly Pidgeon <40374469+reillypidgeon@users.noreply.github.com>
2026-09-01 11:51:08 -0400

    Update unzip_genomes.sh

--- download_scripts/unzip_genomes.sh
+++ download_scripts/unzip_genomes.sh
@@ -14,15 +14,3 @@ echo "Unzipping finished"
 cat unzip_genomes*
 # Remove the output file from the previous job to avoid errors when going through the genomes in later steps
 rm unzip_genomes*
-
-# Add the genome accessions to each fasta file
-if [ -f "../download_scripts/add_accessions.sh" ]; then
-    echo "add_accessions.sh script found"
-    bash ../download_scripts/add_accessions.sh .
-else
-    echo "add_accessions.sh script not found"
-    echo "Exiting script without any fasta modifications"
-    exit 1
-fi
-
-echo "Finished adding genome accessions to fasta files"
