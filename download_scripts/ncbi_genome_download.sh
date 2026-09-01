81ad2795fb8d2d38dc921ff13292d680cf855c24
Reilly Pidgeon <40374469+reillypidgeon@users.noreply.github.com>
2026-09-01 11:52:17 -0400

    Update ncbi_genome_download.sh

--- download_scripts/ncbi_genome_download.sh
+++ download_scripts/ncbi_genome_download.sh
@@ -71,7 +71,17 @@ echo "Running unzip_genomes.sh"
 # Unzip the files
 sbatch --wait ../download_scipts/unzip_genomes.sh
 
-echo "Adding accessions to the fasta files"
-sbatch --wait ../download_scripts/add_accessions.sh .
+
+
+# Add the genome accessions to each fasta file
+if [ -f "../download_scripts/add_accessions.sh" ]; then
+    echo "add_accessions.sh script found"
+    sbatch --wait ../download_scripts/add_accessions.sh .
+    echo "Finished adding genome accessions to fasta files"
+else
+    echo "add_accessions.sh script not found"
+    echo "Exiting script without any fasta modifications"
+    exit 1
+fi
 
 echo "Finished downloading and modifying the desired genomes"
