9fb2b6be4ea2cae58daa6ae7f0747cbd8ea41720
Reilly Pidgeon <40374469+reillypidgeon@users.noreply.github.com>
2026-09-02 10:34:12 -0400

    Update 03_genome_preparation.sh

--- download_scripts/03_genome_preparation.sh
+++ download_scripts/03_genome_preparation.sh
@@ -4,15 +4,21 @@ set -euo pipefail
 
 # Get the path to the script directory and the project directory (bacteria_genome_mining)
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-project_dir="$(dirname ${script_dir})"
+project_dir="$(dirname "${script_dir}")"
 genomes_dir="${project_dir}/genomes"
 
+# Check that the genomes_dir exists
+if [[ ! -d "${genomes_dir}" ]]; then
+    echo "Error: The genomes directory was not found"
+    exit 1
+fi
+
 # Unzip the files
-echo "Unzipping using the unzip_genomes.sh script"
+echo "Unzipping genome files"
 bash "${script_dir}/unzip_genomes.sh" "${genomes_dir}"
 
 # Add the genome accessions to each fasta file
-echo "Adding accessions to fasta headers using the add_accessions.sh script"
+echo "Adding accessions to fasta headers"
 bash "${script_dir}/add_accessions.sh" "${genomes_dir}"
 
 echo "Finished adding genome accessions to fasta files"
