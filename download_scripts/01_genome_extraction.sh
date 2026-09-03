22747c7f2f84d8eb6353e9a2b4d92614381fa084
Reilly Pidgeon <40374469+reillypidgeon@users.noreply.github.com>
2026-09-03 16:37:34 -0400

    Update 01_genome_extraction.sh

--- download_scripts/01_genome_extraction.sh
+++ download_scripts/01_genome_extraction.sh
@@ -5,27 +5,12 @@ set -euo pipefail
 echo "Running $0"
 echo "This script extracts accession and assembly codes from the GTDB release 232 metadata table according to user input"
 
-if [[ "$#" -ne 1 ]]; then
+if [[ "$#" -lt 1 ]]; then
     echo "Error: Invalid number of arguments"
     echo "Required: A taxon according to GTDB taxonomy"
-    echo "Usage: $0 <taxon_string>"
+    echo "Usage: $0 <taxon_string(s)>"
     echo "Example: $0 'g__Enterocloster'"
-    exit 1
-fi
-
-# Initialize variables
-taxon="$1"
-
-if [[ "$taxon" =~ ^[kpcofgs]__ ]]; then 
-    echo "The taxon is valid: ${taxon}"
-else
-    echo "The taxon name is not valid"
-    echo "Please provide a taxon starting with either of the following:"
-    echo "k__ p__ c__ o__ f__ g__ s__"
-    echo "Followed by the Uppercase taxon name"
-    echo "See https://gtdb.ecogenomic.org/tree?r=d__Bacteria"
-    echo "Example: 'g__Enterocloster'"
-    echo "Example: 's__Enterocloster bolteae'"
+    echo "Example: $0 'g__Enterocloster' 's__Hungatella hathewayi'"
     exit 1
 fi
 
@@ -48,7 +33,6 @@ if ! "$python_cmd" -c "import pandas" >/dev/null 2>&1; then
 fi
 
 echo "Using Python: $("$python_cmd" --version)"
-echo "Taxon of interest to extract from metadata: $taxon"
 
 # Define a function that will merge chunked accession tables
 merge_chunked_tables() {
@@ -102,29 +86,45 @@ if [[ ! -f "${metadata_file}" ]]; then
     exit 1
 fi
 
-# Now extract the accessions and assemblies that match a partial string (user input)
-export taxon
+# Check that the string(s) provided are valid according to the GTDB formatting
+for taxon in "$@"; do
+    if [[ "$taxon" =~ ^[kpcofgs]__ ]]; then 
+        echo "The taxon format is valid: ${taxon}"
+    else
+        echo "The taxon name is not valid"
+        echo "Please provide a taxon starting with either of the following:"
+        echo "k__ p__ c__ o__ f__ g__ s__"
+        echo "See https://gtdb.ecogenomic.org/tree?r=d__Bacteria"
+        echo "Example: 'g__Enterocloster'"
+        echo "Example: 's__Enterocloster bolteae'"
+        exit 1
+    fi
+done
+echo "Taxa are valid... Continuing with the workflow"
+
 export metadata_dir
 
-"$python_cmd" << 'EOF'
+# Loop through the user-defined taxa and create genome accession and assembly tables in the metadata directory
+for taxon in "$@"; do
+    export taxon
+    
+    "$python_cmd" << 'EOF'
 import pandas as pd
 import os
-
 taxon_string = os.environ.get('taxon')
 metadata_dir = os.environ.get('metadata_dir')
 
 print(f"Searching the metadata table for: {taxon_string}")
 df = pd.read_csv(f"{metadata_dir}/bac120_metadata_r232_acc.tsv", sep='\t')
 df_taxon = df[df['gtdb_taxonomy'].str.contains(taxon_string, case=False, na=False, regex=False)]
-
 # Now select accession and ncbi_assembly_name columns and export without headers
 df_genomes = df_taxon[['accession', 'ncbi_assembly_name']].copy()
 # Replace spaces with underscores to avoid errors in later steps
 df_genomes['ncbi_assembly_name'] = df_genomes['ncbi_assembly_name'].str.replace(' ', '_', regex=False)
 file_name = f"genomes_{taxon_string}_r232.tsv".replace(" ", "_")
 df_genomes.to_csv(f"{metadata_dir}/{file_name}", sep='\t', header=False, index=False)
-
 EOF
+done
 
 echo "Finished extracting genome accessions and assemblies"
 echo "Can now download genomes relating to $taxon using the 02_genome_download.sh script"
