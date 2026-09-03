b2f3da4847c9616eec61263e9a8c51704a757788
Reilly Pidgeon <40374469+reillypidgeon@users.noreply.github.com>
2026-09-03 17:09:12 -0400

    Update 03_genome_preparation.sh

--- download_scripts/03_genome_preparation.sh
+++ download_scripts/03_genome_preparation.sh
@@ -2,10 +2,18 @@
 
 set -euo pipefail
 
+if [[ "$#" -ne 1 ]]; then
+    echo "Error: Invalid number of arguments"
+    echo "Required: A directory containing the FASTA files to unzip and annotate"
+    echo "Usage: $0 <fasta_directory>"
+    echo "Example: $0 'genomes'"
+    exit 1
+fi
+
 # Get the path to the script directory and the project directory (bacteria_genome_mining)
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 project_dir="$(dirname "${script_dir}")"
-genomes_dir="${project_dir}/genomes"
+genomes_dir="$1"
 
 # Check that the genomes_dir exists
 if [[ ! -d "${genomes_dir}" ]]; then
