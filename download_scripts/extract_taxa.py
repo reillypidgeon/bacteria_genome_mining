5393c86da990b17d0eac85e50c8c9c287f3fc02c
Reilly Pidgeon <40374469+reillypidgeon@users.noreply.github.com>
2026-09-04 09:21:53 -0400

    Update extract_taxa.py

--- download_scripts/extract_taxa.py
+++ download_scripts/extract_taxa.py
@@ -5,6 +5,7 @@ import os
 
 taxon_string = os.environ.get('taxon')
 metadata_dir = os.environ.get('metadata_dir')
+output_dir = os.environ.get('out_dir')
 
 print(f"Searching the metadata table for: {taxon_string}")
 df = pd.read_csv(f"{metadata_dir}/bac120_metadata_r232_acc.tsv", sep='\t')
@@ -14,4 +15,4 @@ df_genomes = df_taxon[['accession', 'ncbi_assembly_name']].copy()
 # Replace spaces with underscores to avoid errors in later steps
 df_genomes['ncbi_assembly_name'] = df_genomes['ncbi_assembly_name'].str.replace(' ', '_', regex=False)
 file_name = f"genomes_{taxon_string}_r232.tsv".replace(" ", "_")
-df_genomes.to_csv(f"{metadata_dir}/{file_name}", sep='\t', header=False, index=False)
+df_genomes.to_csv(f"{output_dir}/{file_name}", sep='\t', header=False, index=False)
