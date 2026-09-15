bd90bcb1ae0b1d0a99d74156975d4a6b2fa58760
Reilly Pidgeon <40374469+reillypidgeon@users.noreply.github.com>
2026-09-15 15:47:55 -0400

    Update 05_mmseqs2_search.sh

--- analysis_scripts/05_mmseqs2_search.sh
+++ analysis_scripts/05_mmseqs2_search.sh
@@ -240,7 +240,7 @@ for subject_fasta in ${subject_fastas}; do
         exit 1
     fi
 	# Check that the subject file is not empty
-	if [[ -z "${subject_fasta}" ]]; then
+	if [[ ! -s "${subject_fasta}" ]]; then
 		echo "${subject_fasta} file is empty. Skipping..."
 		continue
 	fi
