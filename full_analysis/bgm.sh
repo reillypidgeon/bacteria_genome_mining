2ff41de072ebffb4d377bf41340f328baa7c7186
Reilly Pidgeon <40374469+reillypidgeon@users.noreply.github.com>
2026-09-21 11:57:24 -0400

    Update bgm.sh

--- full_analysis/bgm.sh
+++ full_analysis/bgm.sh
@@ -150,13 +150,13 @@ bash ${download_scripts_dir}/01_genome_extraction.sh "${taxa[@]}"
 
 # Step 02
 # Create a single urls_file specific to the taxa specified in the command line arguments
-urls_merged="${out_dir}/urls_"$(echo "${taxa[@]}" | tr ' ' '_')".txt"
+urls_merged="${accessions_dir}/urls_"$(echo "${taxa[@]}" | tr ' ' '_')".txt"
 > "${urls_merged}"
 
 # Download the genomes and append the urls.txt for each iteration of the loop to the $urls_merged file
 for taxon in "${taxa_underscore}"; do
     bash ${download_scripts_dir}/02_genome_download.sh "${accessions_dir}/genomes_${taxon}_r232.tsv"
-    echo ${out_dir}/urls.txt >> "${urls_merged}"
+    echo ${accessions_dir}/urls.txt >> "${urls_merged}"
 done
 
 # Step 03
